42f5c870a60f01887f267b3870113fc7c449800b
Timothy Gallagher <tgallag6@nd.edu>
2020-10-18 16:30:39 -0400

    Final Daily12

--- daily12/scripts/main.js
+++ daily12/scripts/main.js
@@ -1,8 +1,74 @@
 // scripts/main.js
-document.getElementById("myButton").onmouseup = changeLabelText;
+
+var submitButton = document.getElementById('submit-button');
+
+submitButton.onmouseup = getFormInfo;
+
+console.log("Entered script");
+var resetButton = document.getElementById('reset-button');
+
+resetButton.onmouseup = reloader;
+
+function reloader(){
+  console.log("reloader");
+  location.reload();
+}
+
+function getFormInfo(){
+  console.log("get form");
+
+  var greetingText = document.getElementById("greeting-text").value;
+
+  var bodyText = document.getElementById("body-text").value;
+
+  var senderText = document.getElementById("sender-text").value;
+
+  console.log("title: " + greetingText);
+  console.log("title: " + bodyText);
+  console.log("title: " + senderText);
+
+  var footerString = "- ";
+
+  if(document.getElementById("checkbox-1").checked){
+    footerString = footerString + "See you soon! - "
+  }
+
+  if(document.getElementById("checkbox-2").checked){
+    footerString = footerString + "Enjoy your day! - "
+  }
+
+  if(document.getElementById("checkbox-3").checked){
+    footerString = footerString + "Go Irish! - "
+  }
+
+  console.log("title: " + footerString);
+
+  var letterDict = {}
+
+  letterDict["greeting"] = greetingText;
+  letterDict["body"] = bodyText;
+  letterDict["sender"] = senderText;
+  letterDict["footer"] = footerString;
+
+  console.log(letterDict);
 
 
-function changeLabelText() {
-	var label1 = document.getElementById("myLabel");
-	label1.innerHTML = "Paradigms class";
+  displayInfo(letterDict);
+}
+
+function displayInfo(letterDict){
+  console.log("display form");
+
+  var letterGreeting = document.getElementById("greeting");
+  letterGreeting.innerHTML = "Hello " + letterDict["greeting"] + ",";
+
+  var letterBody = document.getElementById("body");
+  letterBody.innerHTML = letterDict["body"];
+
+  var letterFarewell = document.getElementById("farewell");
+  letterFarewell.innerHTML = "Sincerally, " + letterDict["sender"];
+
+  var letterFooter = document.getElementById("footer");
+  letterFooter.innerHTML = letterDict["footer"];
+
 }
